c147a6c8d795da9b28aae25b4e272bf620cdbd34
MRYIYAN <158163522+MRYIYAN@users.noreply.github.com>
2025-04-28 00:41:45 +0200

    [FIX] Login (necesario recrear el usuario en keycloak

--- Synaps-back/laravel/app/Http/Controllers/AuthController.php
+++ Synaps-back/laravel/app/Http/Controllers/AuthController.php
@@ -13,6 +13,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http; 
 use Illuminate\Http\JsonResponse;
 use App\Models\User;
 use Exception;
